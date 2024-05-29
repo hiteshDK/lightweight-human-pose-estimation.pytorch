14f9d104a3ee10b0d48422640c58c757c9066824
hitesh <hk3885@gmail.com>
2024-05-29 20:57:01 +0530

    pose file added main package name

--- modules/pose.py
+++ modules/pose.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-from modules.keypoints import BODY_PARTS_KPT_IDS, BODY_PARTS_PAF_IDS
-from modules.one_euro_filter import OneEuroFilter
+from lightweight_human_pose_estimation.modules.keypoints import BODY_PARTS_KPT_IDS, BODY_PARTS_PAF_IDS
+from lightweight_human_pose_estimation.modules.one_euro_filter import OneEuroFilter
 
 
 class Pose:
